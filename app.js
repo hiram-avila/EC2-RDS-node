119f44962d41d8f50e2486cc1fa79d19026eaf63
Hiram <Hiramavilaa@gmail.com>
2024-05-23 13:18:03 -0600

    removing coments

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-// Importar módulos y configuraciones necesarias
 import express from 'express';
 import pacientesRoutes from './routes/pacientesRoutes.js';
 
-// Inicializar la aplicación Express
 const app = express();
 
-// Configuración de middleware para analizar las solicitudes entrantes en formato JSON
 app.use(express.json());
 
-// Configuración de las rutas para el manejo de pacientes
 app.use('/pacientes', pacientesRoutes);
 
-// Configuración adicional de Express...
-// ...
 
-// Exportar la aplicación configurada
 export default app;
