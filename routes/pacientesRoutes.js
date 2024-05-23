dbcb1ba0a05524843b0b052234949da9ae7f26dd
Hiram <Hiramavilaa@gmail.com>
2024-05-23 13:21:08 -0600

    removing more commetns

--- routes/pacientesRoutes.js
+++ routes/pacientesRoutes.js
@@ -1,19 +1,13 @@
-// Importar el módulo 'express' para manejar las rutas
 import express from 'express';
 const router = express.Router();
 
-// Importar controladores para pacientes desde pacientesController.js
 import {registrarPaciente, eliminarUsuario, iniciarSesion, getPacientes} from '../controllers/pacientesController.js';
 
-// Rutas para el registro y el inicio de sesión de pacientes
-router.post('/registro', registrarPaciente); // Ruta para registrar un nuevo paciente
-router.post('/inicio', iniciarSesion); // Ruta para iniciar sesión de un paciente existente
+router.post('/registro', registrarPaciente); 
+router.post('/inicio', iniciarSesion); 
 
-// Ruta para eliminar un usuario específico
 router.delete('/usuarios/:id', eliminarUsuario);
 
-// Ruta para obtener todos los pacientes
 router.get('/pacientes', getPacientes);
 
-// Exportar el enrutador configurado
 export default router;
