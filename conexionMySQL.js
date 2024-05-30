26e04c840498ebf0e1244bc4a800720eb71e91cb
Hiram <Hiramavilaa@gmail.com>
2024-05-29 22:01:28 -0600

    fix conection

--- conexionMySQL.js
+++ conexionMySQL.js
@@ -1,16 +1,16 @@
-// // conexionMySQL.js
-// import mysql from 'mysql2/promise';
+// conexionMySQL.js
+import mysql from 'mysql2/promise';
 
 
-// const db = await mysql.createConnection({
-//   host: 'database-1.ctt9yfmhhk8v.us-east-2.rds.amazonaws.com',
-//   user: 'admin',
-//   password: 'Choofas13',
-//   database: 'registrousuarios',
-//   port: 3306
+const db = await mysql.createConnection({
+  host: 'database-1.ctt9yfmhhk8v.us-east-2.rds.amazonaws.com',
+  user: 'admin',
+  password: 'Choofas13',
+  database: 'registrousuarios',
+  port: 3306
 
-// });
+});
 
-// console.log("conexion exitosa")
+console.log("conexion exitosa")
 
-// export default db;
+export default db;
--- routes/pacientesRoutes.js
+++ routes/pacientesRoutes.js
@@ -1,14 +1,14 @@
 import express from 'express';
 const router = express.Router();
 
-// import {registrarPaciente, eliminarUsuario, iniciarSesion, getPacientes} from '../controllers/pacientesController.js';
+import {registrarPaciente, eliminarUsuario, iniciarSesion, getPacientes} from '../controllers/pacientesController.js';
 
-// router.post('/registro', registrarPaciente); 
-// router.post('/inicio', iniciarSesion); 
+router.post('/registro', registrarPaciente); 
+router.post('/inicio', iniciarSesion); 
 
-// router.delete('/usuarios/:id', eliminarUsuario);
+router.delete('/usuarios/:id', eliminarUsuario);
 
-// router.get('/pacientes', getPacientes);
+router.get('/pacientes', getPacientes);
 router.get('/', (req, res) => {
     res.json({message: 'test'});
 });
