702bef8992141cf0d0dbdb2c21ee86ceb277a651
Hiram <Hiramavilaa@gmail.com>
2024-05-29 20:08:52 -0600

    resolve issue conexion

--- conexionMySQL.js
+++ conexionMySQL.js
@@ -3,9 +3,9 @@ import mysql from 'mysql2/promise';
 
 
 const db = await mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
+  host: 'database-1.ctt9yfmhhk8v.us-east-2.rds.amazonaws.com',
+  user: 'admin',
+  password: 'Choofas13',
   database: 'registrousuarios',
   port: 3306
 
