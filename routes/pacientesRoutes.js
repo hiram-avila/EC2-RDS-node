a9a3b80b2df7a305c9d67a057ac6fdcc67ecbfc2
Hiram <Hiramavilaa@gmail.com>
2024-06-01 16:09:19 -0600

    fix issue route

--- routes/pacientesRoutes.js
+++ routes/pacientesRoutes.js
@@ -11,7 +11,7 @@ router.delete('/usuarios/:id', eliminarUsuario);
 router.get('/pacientes', getPacientes);
 
 router.get('/', (req, res) => {
-    res.json({message: 'se ha  realizada con exito!!'});
+    res.json({message: 'se ha  realizada con exito el test!!'});
 });
 
 export default router;
