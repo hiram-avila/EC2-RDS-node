0ef03b06b5704aa70aed1c4d9ec4c5aaab44ce63
Hiram <Hiramavilaa@gmail.com>
2024-06-01 14:52:35 -0600

    new commit

--- routes/pacientesRoutes.js
+++ routes/pacientesRoutes.js
@@ -11,7 +11,7 @@ router.delete('/usuarios/:id', eliminarUsuario);
 router.get('/pacientes', getPacientes);
 
 router.get('/', (req, res) => {
-    res.json({message: 'prueba realizada con exito!'});
+    res.json({message: 'test realizada con exito!'});
 });
 
 export default router;
