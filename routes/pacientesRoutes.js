e320732c09dfcca8a57f159c7354214c47585589
Hiram <Hiramavilaa@gmail.com>
2024-06-20 15:33:31 -0600

    changes in routes

--- routes/pacientesRoutes.js
+++ routes/pacientesRoutes.js
@@ -11,7 +11,7 @@ router.delete('/usuarios/:id', eliminarUsuario);
 router.get('/pacientes', getPacientes);
 
 router.get('/', (req, res) => {
-    res.json({message: 'errores corregidos en test third part'});
+    res.json({message: 'errores corregidos'});
 });
 
 export default router;
