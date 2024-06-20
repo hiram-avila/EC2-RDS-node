eb733e1dde40300279cedab5cef84987ef487831
Hiram <Hiramavilaa@gmail.com>
2024-06-20 17:21:39 -0600

    test 1.0

--- routes/pacientesRoutes.js
+++ routes/pacientesRoutes.js
@@ -1,14 +1,7 @@
 import express from 'express';
 const router = express.Router();
 
-import {registrarPaciente, eliminarUsuario, iniciarSesion, getPacientes} from '../controllers/pacientesController.js';
 
-router.post('/registro', registrarPaciente); 
-router.post('/inicio', iniciarSesion); 
-
-router.delete('/usuarios/:id', eliminarUsuario);
-
-router.get('/pacientes', getPacientes);
 
 router.get('/', (req, res) => {
     res.json({message: 'errores corregidos'});
