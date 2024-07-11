f3792d7a1ba4c70a0354abf2a3b0e583f0d2d5b8
Hiram <Hiramavilaa@gmail.com>
2024-07-10 22:28:45 -0600

    test no3

--- tests/pacientesController.spec.js
+++ tests/pacientesController.spec.js
@@ -1,11 +1,28 @@
-
+import app from '../app'; // Asegúrate de importar tu aplicación express o servidor
+import request from 'supertest';
 
 describe('PacienteController', () => {
-  const sum = (a, b) => a + b;
 
-  test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
+  test('debería registrar a un usuario', async () => {
+    // Supongamos que tienes un usuario de prueba en tu base de datos de prueba
+    const usuarioPrueba = {
+      correo: 'test@gmail.com',
+      contrasena: '123456',
+      nombre: 'test',
+      edad: 10
+    };
+    // Realiza una solicitud POST simulada a la ruta de inicio de sesión
+    const response = await request(app)
+      .post('/pacientes/registro')
+      .send(usuarioPrueba);
+
+    // Verifica que la respuesta tenga un código de estado 200
+    expect(response.statusCode).toBe(200);
+
+    // Verifica que la respuesta contenga un token y un mensaje
+    
+    expect(response.body).toHaveProperty('message', 'Paciente registrado con éxito');
+});
 
     
  
